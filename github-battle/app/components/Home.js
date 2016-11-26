7894c0bf43b460ae11ffe23d54231b5b3c7c942d
Doug Wells <dougwells@gmail.com>
2016-11-26 16:52:21 -0700

    React. Github Battle. Set up React Router

---
+++ github-battle/app/components/Home.js
@@ -0,0 +1,13 @@
+var React = require('react')
+
+var Home = React.createClass({
+  render: function(){
+    return (
+      <div>
+        Welcome to our home page!
+      </div>
+    )
+  }
+})
+
+module.exports = Home;
--- github-battle/app/index.js
+++ github-battle/app/index.js
@@ -1,61 +1,9 @@
 var React = require('react');
 var ReactDom = require('react-dom');
+var routes = require('./config/routes');
 
-var USER_DATA = {
-  name: 'Tyler McGinnis',
-  username: 'tylermcginnis',
-  image: 'https://avatars0.githubusercontent.com/u/2933430?v=3&s=460'
-}
-
-var HelloWorld = React.createClass({
-  render: function(){
-    return (
-      <div>Hello {this.props.name}!</div>
-    )
-  }
-})
-
-var ProfilePick = React.createClass({
-  render: function(){
-    return (
-      <img src={this.props.imageUrl} style={{height:100, width:100}} />
-    )
-  }
-});
-
-var ProfileLink = React.createClass({
-  render: function(){
-    return (
-      <div>
-        <a href = {'https://github.com/' + this.props.userName}>
-        {this.props.userName}
-        </a>
-      </div>
-    )
-  }
-});
-
-var ProfileName = React.createClass({
-  render: function(){
-    return (
-      <div>{this.props.name}</div>
-    )
-  }
-});
-
-var Avatar = React.createClass({
-  render: function(){
-    return (
-      <div>
-        <ProfilePick imageUrl={this.props.user.image} />
-        <ProfileName name={this.props.user.name} />
-        <ProfileLink userName={this.props.user.username} />
-      </div>
-    )
-  }
-})
 
 ReactDom.render(
-  <Avatar user={USER_DATA} />,
+  routes,
   document.getElementById('app')
-)
+);
